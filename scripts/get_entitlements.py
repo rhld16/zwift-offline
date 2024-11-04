f27b6f9ed9e1cb6ddccb5693258c79000b9e259b
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2024-11-04 11:05:03 -0300

    Add entitlements

---
+++ scripts/get_entitlements.py
@@ -0,0 +1,18 @@
+import json
+
+with open('C:\\Program Files (x86)\\Zwift\\ZwiftApp.exe', 'rb') as f:
+    za = f.read()
+data = []
+s = 0
+while True:
+    p = za.find(b'\x45\x4E\x54\x49\x54\x4C\x45\x4D\x45\x4E\x54\x5F', s)
+    if p != -1:
+        i = p
+        while za[i] != 0:
+            i += 1
+        data.append(za[p:i].decode("utf-8"))
+        s = i
+    else:
+        break
+with open('../data/entitlements.txt', 'w') as f:
+    json.dump(sorted(data), f, indent=2)
